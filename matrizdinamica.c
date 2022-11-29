a0c1049ee808dcb8001e9f928feecbea9c7af2a9
Alex Náger <51228484+aleexnager@users.noreply.github.com>
2022-11-29 12:20:25 +0100

    Update matrizdinamica.c

--- matrizdinamica.c
+++ matrizdinamica.c
@@ -12,23 +12,21 @@ int main(int argc, char *argv[])
     p = atoi(argv[2]); /*num columnas*/
 
     /*Memoria dinamica*/
-    matriz = (long int **)calloc(m, sizeof(long int *));
+    matriz = (long int **)calloc(m, sizeof(long int));
     if (matriz == NULL) exit(71);
     for (i = 0; i < m; ++i)
     {
-        matriz[i] = (long int *)calloc(p, sizeof(long int *));
+        matriz[i] = (long int *)calloc(p, sizeof(long int));
         if (matriz[i] == NULL) exit(71);
     }
 
     /*Asignacion valores*/
     for (i = 0; i < m; ++i)
         for (j = 0; j < p; ++j)
+        {
             matriz[i][j] = (i == 0 || j == 0) ? 1 : matriz[i - 1][j] + matriz[i][j - 1];
-
-    /*prints*/
-    for (i = 0; i < m; ++i)
-        for (j = 0; j < p; ++j) 
             (j == p-1) ? printf("%li\n", matriz[i][j]) : printf("%li\t", matriz[i][j]); /*para ultimo elem : para el resto de elem*/
+        }
 
     /*Liberacion menoria dinamica*/
     for (i = 0; i < m; ++i)
